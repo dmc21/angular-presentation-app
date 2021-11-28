a4bfd21441bbd5aba4cbf7a330b9eb900ed64b4e
dmc21 <davidmoracaceres20@gmail.com>
2021-11-28 10:54:55 +0100

    commited

--- src/app/cabecera/cabecera.component.ts
+++ src/app/cabecera/cabecera.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component ({
   selector: 'app-lacabecera',
-  templateUrl: './cabecera.component.html'
+  templateUrl: './cabecera.component.html',
+  styleUrls: ['./cabecera.component.css']
 })
 
 export class CabeceraComponent {
